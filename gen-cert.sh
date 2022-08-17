1061f69b770a51e379fc71211e011628ce2dcb3a
Sebastien Waegeneire <sebastien@waegeneire.com>
2022-08-17 15:56:21 +0200

    save

--- gen-cert.sh
+++ gen-cert.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
-if [ -z "$1" ]
-  then
-    echo "no username supplied"
-else 
-  openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-    -keyout "$1".42.fr.key \
-    -out "$1".42.fr.crt \
-    -subj "/CN=$1.42.fr"
-fi
+SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+  -keyout "$SCRIPT_PATH/srcs/requirements/nginx/ssl/$USER".42.fr.key \
+  -out "$SCRIPT_PATH/srcs/requirements/nginx/ssl/$USER".42.fr.crt \
+  -subj "/CN=$USER.42.fr"
