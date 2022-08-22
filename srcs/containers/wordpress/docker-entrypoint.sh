7137540849c218ee68a77f1715dbd19e9423e646
Sebastien Waegeneire <sebastien@waegeneire.com>
2022-08-22 10:28:25 -0400

    save

--- srcs/containers/wordpress/docker-entrypoint.sh
+++ srcs/containers/wordpress/docker-entrypoint.sh
@@ -11,16 +11,18 @@ if [ "$1" = 'wordpress' ]; then
 
   if [ ! -e $DATA_DIR/swaegene.42.fr/public/index.php ]; then
     wp_cli() {
-      gosu www-data php wp-cli.phar $@ --path=$DATA_DIR/swaegene.42.fr/public
+      gosu www-data php $DATA_DIR/wp-cli.phar $@
     }
     # download wordpress-cli
     curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+    # cd to wordpress install dir
+    cd $DATA_DIR/swaegene.42.fr/public
     # download wordpress
     wp_cli core download 
     # create config file
     wp_cli config create --dbname=wordpress --dbuser=wordpress --dbpass=$(< /run/secrets/mariadb_wordpress_password) --dbhost=mariadb:3306
     # install wordpress
-    wp_cli core install --url=https://swaegene.42.fr --title=Inception --admin_user=swaegene --admin_password=$(< /run/secrets/swaegene.42.fr_admin_password) --admin_email=swaegene@student.42mulhouse.fr --skip-email
+    wp_cli core install --url="https://swaegene.42.fr" --title=Inception --admin_user=swaegene --admin_password=$(< /run/secrets/swaegene.42.fr_admin_password) --admin_email=swaegene@student.42mulhouse.fr --skip-email
     # create user
     wp_cli user create user user@example.com --role=author --user_pass=$(< /run/secrets/swaegene.42.fr_user_password)
     # TODO remove wordpress-cli 
