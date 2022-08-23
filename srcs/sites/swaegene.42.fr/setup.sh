a9df550b4c168a814cab676c36113d65882c3d53
Sebastien Waegeneire <sebastien@waegeneire.com>
2022-08-23 08:14:53 -0400

    save

--- srcs/sites/swaegene.42.fr/setup.sh
+++ srcs/sites/swaegene.42.fr/setup.sh
@@ -24,7 +24,7 @@ if [ ! -e $DATA_DIR/swaegene.42.fr/wordpress ]; then
   gosu www-data wp core download 
   gosu www-data wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost=$DB_HOST
   gosu www-data wp config set WP_SITEURL $URL
-  gosu www-data wp core install --url="$URL" --title="$TITLE" --admin_user=$ADMIN_USER --admin_password= --admin_email=$ADMIN_EMAIL --skip-email
+  gosu www-data wp core install --url="$URL" --title="$TITLE" --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASS --admin_email=$ADMIN_EMAIL --skip-email
   gosu www-data wp user create $USER_USER $USER_EMAIL --role=author --user_pass=$USER_PASS
 
   gosu www-data wp config set WP_REDIS_CLIENT phpredis
@@ -33,4 +33,5 @@ if [ ! -e $DATA_DIR/swaegene.42.fr/wordpress ]; then
   gosu www-data wp config set WP_REDIS_PORT $REDIS_PORT
   gosu www-data wp config set WP_REDIS_PASSWORD $REDIS_PASS
   gosu www-data wp plugin install redis-cache --activate
+  gosu www-data wp redis enable
 fi
