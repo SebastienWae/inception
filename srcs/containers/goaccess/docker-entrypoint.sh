bc1755ee45989b8a9960a53a4e0800e187c5604b
Sebastien Waegeneire <sebastien@gmail.com>
2022-08-26 10:37:43 -0400

    save

--- srcs/containers/goaccess/docker-entrypoint.sh
+++ srcs/containers/goaccess/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 if [ "$1" = 'goaccess' ]; then
   # start goaccess
-  exec goaccess $DATA_DIR/logs/nginx/access.log -o $DATA_DIR/goaccess.42.fr/index.html --log-format=COMBINED --real-time-html
+  exec goaccess $DATA_DIR/logs/nginx/access.log -o $DATA_DIR/goaccess.42.fr/index.html
 fi
 
 exec "$@"
