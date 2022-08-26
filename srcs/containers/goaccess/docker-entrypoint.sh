012c43c98091833b07ac125153a66ec2cc4fdbc5
Sebastien Waegeneire <sebastien@gmail.com>
2022-08-26 11:01:05 -0400

    save

--- srcs/containers/goaccess/docker-entrypoint.sh
+++ srcs/containers/goaccess/docker-entrypoint.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 if [ "$1" = 'goaccess' ]; then
+  # create goaccess.42.fr dir
+  mkdir -p $DATA_DIR/goaccess.42.frs
+
   # start goaccess
   exec goaccess $DATA_DIR/logs/nginx/access.log -o $DATA_DIR/goaccess.42.fr/index.html
 fi
