569fc5f18527a9779fbde53197b36e7c7649a712
Sebastien Waegeneire <sebastien@waegeneire.com>
2022-08-30 08:37:24 -0400

    save

--- srcs/containers/static/docker-entrypoint.sh
+++ srcs/containers/static/docker-entrypoint.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
 if [ "$1" = 'static' ]; then
-  if [ ! -d "$DATA_DIR/static.42.fr" ]; then
-    git clone https://github.com/SebastienWae/debugging-42.git static.42.fr
-  fi
-
   cd static.42.fr
   npm install
 
